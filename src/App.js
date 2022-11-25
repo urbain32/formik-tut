78e2028865f8b5b8d91c7cbaf1194af7a8e1527f
urbain32 <urbainq32@gmail.com>
2022-11-25 09:45:10 +0200

    how to use formik

--- src/App.js
+++ src/App.js
@@ -3,28 +3,44 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className='app'>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <TextField
           id='firstName'
           name='firstName'
           label='first name'
           margin='normal'
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
         />
         <TextField
           id='lastName'
           name='lastName'
           label='last name'
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
           margin='normal'
         />
         <TextField
           id='email'
           name='email'
           label='email address'
+          onChange={formik.handleChange}
+          value={formik.values.email}
           margin='normal'
         />
-        <Button variant='outlined' type='submit'>
+        <Button variant='contained' type='submit'>
           Submit
         </Button>
       </form>
